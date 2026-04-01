b801ed858fea2b80c7c67588227124d978ccc3a2
asish <beingasishkumar@gmail.com>
2026-04-02 01:28:55 +0530

    swagger configs added

--- src/main/java/com/ecommerce/common/boot/ECommBootApplication.java
+++ src/main/java/com/ecommerce/common/boot/ECommBootApplication.java
@@ -1,13 +1,12 @@
 package com.ecommerce.common.boot;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.annotation.AliasFor;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.AliasFor;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -15,15 +14,15 @@ import java.lang.annotation.Target;
 @SpringBootApplication
 public @interface ECommBootApplication {
 
-    @AliasFor(annotation = SpringBootApplication.class)
-    Class<?>[] exclude() default {};
+  @AliasFor(annotation = SpringBootApplication.class)
+  Class<?>[] exclude() default {};
 
-    @AliasFor(annotation = SpringBootApplication.class)
-    String[] excludeName() default {};
+  @AliasFor(annotation = SpringBootApplication.class)
+  String[] excludeName() default {};
 
-    @AliasFor(annotation = SpringBootApplication.class)
-    String[] scanBasePackages() default {};
+  @AliasFor(annotation = SpringBootApplication.class)
+  String[] scanBasePackages() default {};
 
-    @AliasFor(annotation = SpringBootApplication.class)
-    Class<?>[] scanBasePackageClasses() default {};
+  @AliasFor(annotation = SpringBootApplication.class)
+  Class<?>[] scanBasePackageClasses() default {};
 }
