baed2ca4950432392ba73beb9a87c95b3db3e975
asish <beingasishkumar@gmail.com>
2026-04-02 17:32:52 +0530

    default local date time format

---
+++ src/main/java/com/ecommerce/common/constants/DateTimeConstants.java
@@ -0,0 +1,12 @@
+package com.ecommerce.common.constants;
+
+public final class DateTimeConstants {
+
+  private DateTimeConstants() {
+    // Prevent instantiation
+  }
+
+  public static final String DATETIME_FORMAT = "dd/MM/yyyy hh:mm:ss a";
+  public static final String DATE_FORMAT = "dd/MM/yyyy";
+  public static final String TIME_FORMAT = "hh:mm:ss a";
+}
