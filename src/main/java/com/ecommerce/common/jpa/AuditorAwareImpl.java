0cb35a05f45f2ff03b5799279a5ece66c112c7bc
asish <beingasishkumar@gmail.com>
2026-04-03 16:49:56 +0530

    base entity setup

--- src/main/java/com/ecommerce/common/jpa/AuditorAwareImpl.java
+++ src/main/java/com/ecommerce/common/jpa/AuditorAwareImpl.java
@@ -1,13 +1,14 @@
 package com.ecommerce.common.jpa;
 
 import java.util.Optional;
-import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
-  public @NonNull Optional<String> getCurrentAuditor() {
+  @NonNull
+  public Optional<String> getCurrentAuditor() {
     return Optional.of("SYSTEM");
   }
 }
