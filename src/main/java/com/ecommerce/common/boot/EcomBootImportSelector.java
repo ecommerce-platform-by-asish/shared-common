8426e006e90b95a1ad52df2be22f51af710b65e1
asish <beingasishkumar@gmail.com>
2026-04-04 04:54:17 +0530

    common actuator config added

--- src/main/java/com/ecommerce/common/boot/EcomBootImportSelector.java
+++ src/main/java/com/ecommerce/common/boot/EcomBootImportSelector.java
@@ -5,13 +5,14 @@ import com.ecommerce.common.config.RedisCacheAutoConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 class EcomBootImportSelector implements ImportSelector {
 
   @Override
-  public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+  public String @NonNull [] selectImports(AnnotationMetadata importingClassMetadata) {
     Map<String, Object> attributes =
         importingClassMetadata.getAnnotationAttributes(EcomBootApplication.class.getName());
 
@@ -21,8 +22,12 @@ class EcomBootImportSelector implements ImportSelector {
 
     List<String> imports = new ArrayList<>();
 
-    if ((boolean) attributes.getOrDefault("enableOpenApi", true)) {
+    if ((boolean) attributes.getOrDefault("enableOpenApi", false)) {
       imports.add(OpenApiAutoConfiguration.class.getName());
+    }
+
+    if ((boolean) attributes.getOrDefault("enableActuator", false)) {
+      imports.add("com.ecommerce.common.config.ActuatorAutoConfiguration");
     }
 
     if ((boolean) attributes.getOrDefault("enableCaching", false)) {
